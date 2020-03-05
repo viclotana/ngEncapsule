41b3990104bd788bddf691e857133e1442009894
Nwose Lotanna <lotanna.nwose@meltwater.org>
2020-03-05 08:20:22 +0000

    final commit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+
+@Component({
+  selector: "app-test",
+  templateUrl: "./test.component.html",
+  styleUrls: ["./test.component.css"],
+  encapsulation: ViewEncapsulation.ShadowDom
+})
+export class TestComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
